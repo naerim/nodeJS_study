232c58e68f5942879b0991e472e8ddeef13aef70
naerim <naerim1119@gmail.com>
2021-08-02 21:36:31 +0900

    set morgan, cookie-parser, express-session, dotenv

--- 6-learn_express/app.js
+++ 6-learn_express/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config(); // dotenv는 .env 파일을 읽어서 process.env로 만든다., 보안 설정의 편의성
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.use(margan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
 
 // app.use(미들웨어)
 app.use((req, res, next) => {
