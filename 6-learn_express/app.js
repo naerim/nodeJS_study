12de01068156dc8dd7a00c9c65ecc91c2e1a4645
naerim <naerim1119@gmail.com>
2021-08-04 22:10:43 +0900

    router 객체

--- 6-learn_express/app.js
+++ 6-learn_express/app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config(); // dotenv는 .env 파일을 읽어서 process.env로 만든다., 보안 설정의 편의성
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -24,6 +26,12 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use((req,res,next) => {
+    res.status(404).send('Not Found');
+});
 
 const multer = require('multer');
 const fs = require('fs');
